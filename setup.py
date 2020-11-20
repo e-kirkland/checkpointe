fc65ad06edd681d3529427824b96646b94dd4416
Eddie Kirkland <qjg0@cdc.gov>
2020-11-20 07:19:39 -0500

    removing tracemalloc

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 
 INSTALL_REQUIRES = [
-      'datetime',
-      'pytracemalloc'
+      'datetime'
 ]
 
 setup(name=PACKAGE_NAME,
